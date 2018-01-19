dd4f5ea707140f0612a63de13e521c3ad7edc3c1
安东 <zhou_andong@sohu.com>
2018-01-19 13:09:53 +0800

    springframework版本升级

--- src/main/java/cn/no7player/controller/HelloController.java
+++ src/main/java/cn/no7player/controller/HelloController.java
@@ -8,10 +8,11 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class HelloController {
 
-    @RequestMapping("/hello")
-    public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
-        model.addAttribute("name", name);
-        return "hello";
-    }
-    
+	@RequestMapping("/hello")
+	public String greeting(@RequestParam(value = "name", required = false, defaultValue = "World") String name,
+			Model model) {
+		model.addAttribute("name", name);
+		return "hello";
+	}
+
 }
